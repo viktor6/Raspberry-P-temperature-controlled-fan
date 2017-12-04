0ca6654833f1d1cfe6e5a248ab47aea18b145656
viktor6 <viktor6@ukr.net>
2017-12-04 23:45:22 +0200

    1
    1

--- fan.sh
+++ fan.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
-echo "2" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio2/direction
+echo "17" > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio17/direction
 timestamp() {
 date +"%Y-%m-%d %T"
 }
+# crontab -e
+#* * * * * /home/pi/fan/./fan.sh
+#
+
 LOGDIR="/home/pi/fan/fan.log"
 VALUE=60
 TEMP=`vcgencmd measure_temp | cut -c6,7`
-STATUS=`cat /sys/class/gpio/gpio2/value`
+STATUS=`cat /sys/class/gpio/gpio17/value`
 #echo `timestamp` " Info: Temperature: $TEMP">>$LOGDIR
 #echo `timestamp` " Info: Temperature: $TEMP" " STATUS: $STATUS">>$LOGDIR
 if [ $TEMP -ge $VALUE ] && [ $STATUS -eq 0 ]
 then
 #echo `timestamp` " Warning: Fan started.">>$LOGDIR
-echo "1">/sys/class/gpio/gpio2/value
+echo "1">/sys/class/gpio/gpio17/value
 elif [ $TEMP -lt $VALUE ] && [ $STATUS -eq 1 ]
 then
 #echo `timestamp` " Warning: Fan stopped.">>$LOGDIR
-echo "0">/sys/class/gpio/gpio2/value
+echo "0">/sys/class/gpio/gpio17/value
 fi
 
